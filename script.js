f7c1a009948503f5a8f1e3b5bd144ad4b59810a0
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-05-26 01:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getFormvalue() {
-    //Write your code here
-
+    let input1 = document.getElementsByTagName("input")[0].value;
+	let input2 = document.getElementsByTagName("input")[1].value;
+	alert(input1 + " " + input2);
 }
